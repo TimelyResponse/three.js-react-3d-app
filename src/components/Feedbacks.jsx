05a2a142ec01e654908deb777c5a377da975210f
TimelyResponse <148927425+TimelyResponse@users.noreply.github.com>
2023-11-21 18:51:42 +0900

    update: feed back style

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -48,12 +48,12 @@ const Feedbacks = () => {
       <div
         className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}
       >
-        <motion.div variants={textVariant()}>
+        <motion.div variants={ textVariant()}>
           <p className={styles.sectionSubText}>What others say</p>
           <h2 className={styles.sectionHeadText}>Testimonials.</h2>
         </motion.div>
       </div>
-      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+      <div className={`${styles.paddingX} -mt-20  pb-14 flex  flex-wrap gap-9`}>
         {testimonials.map((testimonial, index) => (
           <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
         ))}
